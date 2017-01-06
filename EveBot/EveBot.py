9248ee66e29a16c8565bc432a01e509a867541a0
Stevenator1 <stevenkneisler@gmail.com>
2017-01-05 23:30:49 -0500

    Fixed file placement stupidity

--- EveBot/EveBot.py
+++ EveBot/EveBot.py
@@ -17,22 +17,22 @@ class Mycog:
 
 
 
-	@commands.command()
-	async def evepraisal(self, *text):
-	    """Run evepraisal and spit out result"""
+    @commands.command()
+    async def evepraisal(self, *text):
+        """Run evepraisal and spit out result"""
 
-	    #Your code will go here
-	    url = "http://evepraisal.com/estimate/post" #build the web adress
+        #Your code will go here
+        url = "http://evepraisal.com/estimate/post" #build the web adress
 
-		params = {'raw_textarea': join(text[0..len(text)], 'market': '30000142', 'load_full': '1'}
-		async with session.get(url,
-		                       params=params) as resp:
-		    soupObject = BeautifulSoup(await response.text(), "html.parser")
-	    try:
-	        results = soupObject.find(id='results').get_text()
-	        await self.bot.say(results)
-	    except:
-	        await self.bot.say("Failed.")
+        params = {'raw_textarea': join(text[0..len(text)], 'market': '30000142', 'load_full': '1'}
+        async with session.get(url,
+                               params=params) as resp:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            results = soupObject.find(id='results').get_text()
+            await self.bot.say(results)
+        except:
+            await self.bot.say("Failed.")
 
 
 
