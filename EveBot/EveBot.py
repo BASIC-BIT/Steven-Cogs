eafd012774815c4ab0300f368579e6c336799ea1
Stevenator1 <stevenkneisler@gmail.com>
2017-01-06 02:18:09 -0500

    Test

--- EveBot/EveBot.py
+++ EveBot/EveBot.py
@@ -56,8 +56,8 @@ class Mycog:
                 sellvalue = results_split[9].split('.')[0]
                 buyvalue = results_split[10].split('.')[0]
                 sizem = results_split[11]
-                sizem = size.replace('m3','')
-                results_joined = ' '.join(results_split[1:4]) + ':\t' + "{:,.0f}".format(float(sellvalue)) + ' isk\n' + ' '.join(results_split[4:7]) + ':\t' + "{:,.0f}".format(float(buyvalue)) + ' isk\n' + ' '.join(results_split[7:9]) + ':\t' + "{:,.2f}".format(float(sizem)) + " m3"
+                sizem = sizem.replace('m3','')
+                results_joined = ' '.join(results_split[1:4]) + ':\t' + "{:,}".format(int(sellvalue)) + ' isk\n' + ' '.join(results_split[4:7]) + ':\t' + "{:,.0f}".format(int(buyvalue)) + ' isk\n' + ' '.join(results_split[7:9]) + ':\t' + "{:,.2f}".format(float(sizem)) + " m3"
                 await self.bot.say('Results: \n' + results_joined)
             except:
                 await self.bot.say("Failed.")
