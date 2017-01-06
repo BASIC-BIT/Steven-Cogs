8f084ee97b6357c94122ea5ae468e60ed9f641f3
Stevenator1 <stevenkneisler@gmail.com>
2017-01-06 01:58:45 -0500

    Finalizing pretyfying bug fixes

--- EveBot/EveBot.py
+++ EveBot/EveBot.py
@@ -53,7 +53,9 @@ class Mycog:
                 for counter in range(1,100):
                     results = results.replace('  ', ' ')
                 results_split = results.split(' ')
-                results_joined = ' '.join(results_split[1:4]) + ': ' + "{:,.2f}".format(float(results_split[9])) + ' isk\n' + ' '.join(results_split[4:7]) + ': ' + "{:,.2f}".format(float(results_split[10])) + ' isk\n' + ' '.join(results_split[7:9]) + ': ' + results_split[11]
+                sizem = results_split[11]
+                sizem = size.replace('m3','')
+                results_joined = ' '.join(results_split[1:4]) + ':\t' + "{:,}".format(float(results_split[9])) + ' isk\n' + ' '.join(results_split[4:7]) + ':\t' + "{:,}".format(float(results_split[10])) + ' isk\n' + ' '.join(results_split[7:9]) + ':\t' + "{:,}".format(float(sizem))+" m3"
                 await self.bot.say('Results: \n' + results_joined)
             except:
                 await self.bot.say("Failed.")
