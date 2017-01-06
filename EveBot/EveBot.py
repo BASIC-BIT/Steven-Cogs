53a583092d1b9aeeb0efba799a1884c3cb10efd1
Stevenator1 <stevenkneisler@gmail.com>
2017-01-06 00:55:02 -0500

    Debug Fixes

--- EveBot/EveBot.py
+++ EveBot/EveBot.py
@@ -36,15 +36,15 @@ class Mycog:
         items_split = items.split(" ")
         items_rejoin = " ".join(items_split[1:])
 
-        params = {'raw_textarea': items_rejoin, 'market': '30000142', 'load_full': '1'}
+        params = {'raw_paste': items_rejoin, 'market': '30000142'}
         await self.bot.say(items_rejoin)
         async with aiohttp.ClientSession() as session:
             async with session.post(url,
                                    data=params) as response:
                 soupObject = BeautifulSoup(await response.text(), "html.parser")
             try:
-                #results = soupObject.find(id='results').get_text()
-                results = soupObject.body.get_text()
+                results = soupObject.find(id='results').get_text()
+                #results = soupObject.body.get_text()
                 await self.bot.say(results)
             except:
                 await self.bot.say("Failed.")
