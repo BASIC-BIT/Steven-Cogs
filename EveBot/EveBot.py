ae5ad548dd7ae22252386105894b61514185b666
Stevenator1 <stevenkneisler@gmail.com>
2017-01-06 02:19:40 -0500

    Debug (ACTUALLY WORKING YAYYYYYYY ALMOST DONE!!!)

--- EveBot/EveBot.py
+++ EveBot/EveBot.py
@@ -57,7 +57,7 @@ class Mycog:
                 buyvalue = results_split[10].split('.')[0]
                 sizem = results_split[11]
                 sizem = sizem.replace('m3','')
-                results_joined = ' '.join(results_split[1:4]) + ':\t' + "{:,}".format(int(sellvalue)) + ' isk\n' + ' '.join(results_split[4:7]) + ':\t' + "{:,.0f}".format(int(buyvalue)) + ' isk\n' + ' '.join(results_split[7:9]) + ':\t' + "{:,.2f}".format(float(sizem)) + " m3"
+                results_joined = ' '.join(results_split[1:4]) + ':\t' + "{:,.2f}".format(float(sellvalue)) + ' isk\n' + ' '.join(results_split[4:7]) + ':\t' + "{:,.2f}".format(float(buyvalue)) + ' isk\n' + ' '.join(results_split[7:9]) + ':\t\t' + "{:,.2f}".format(float(sizem)) + " m3"
                 await self.bot.say('Results: \n' + results_joined)
             except:
                 await self.bot.say("Failed.")
